c3e548d548238149e0c9a5d7a21a2028db0d7ca9
jayhun0521 <jayhun21@gmail.com>
2022-11-27 22:26:06 -0500

    updating JWT files

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,27 @@
 //controller actions
 const User = require('../models/User');
 
+//handle errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email: '', password:'' };
+
+// duplicate error code
+if (err.code === 11000) {
+    errors.email = 'that emial is already registered';
+    return errors;
+}
+
+
+//validation errors
+if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({properties}) => {
+        errors[properties.path] = properties.message;
+    });
+ }
+ 
+ return errors;
+}
 
 module.exports.signup_get = (req, res) => {
     res.render('signup');
@@ -18,12 +39,11 @@ module.exports.signup_post = async (req, res) => {
         res.status(201).json(user);
     }
     catch(err){
-        console.log(err);
-        res.status(400).send("error, user wasn't created")
+        const errors = handleErrors(err);
+        res.status(400).send({ errors });
     }
-    console.log(email, password);
-    res.send('new signup');
 }
+
 
 module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
--- models/User.js
+++ models/User.js
@@ -1,17 +1,31 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter an email'],
         unique: true, 
-        lowercase: true
+        lowercase: true,
+        validate: [ isEmail, 'Please enter a valid email']
     },
     password: {
         type: String,
-        required: true,
-        minlength: 6
+        required: [true, 'Please enter an password'],
+        minlength: [6, 'Minimum password length is 6 characters']
     },
+});
+
+// fire a function after a doc saved to db
+userSchema.post('save', function (doc, next) {
+  console.log('new user was created & saved', doc);
+    next();
+});
+
+// fire a functio before doc saved to db
+userSchema.pre('save', function (next) {
+    console.log('user about to be saved', this);
+    next();
 });
 
 const User = mongoose.model('user', userSchema);
