059f17e899e5a516b6a3ec4ea4fe3379a8c35f12
jayhun0521 <jayhun21@gmail.com>
2022-11-28 00:15:43 -0500

    updating jwt-lesson-1

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 //controller actions
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 //handle errors
 const handleErrors = (err) => {
@@ -23,6 +24,13 @@ if (err.message.includes('user validation failed')) {
  return errors;
 }
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({ id }, 'net ninja secret', {
+        expiresIn: maxAge
+    });
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -36,7 +44,9 @@ module.exports.signup_post = async (req, res) => {
 
     try{
         const user = await User.create({ email, password });
-        res.status(201).json(user);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(201).json({ user: user._id });
     }
     catch(err){
         const errors = handleErrors(err);
